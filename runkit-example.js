b0c503354820936904203f5fbb66c9ae9edff0d1
Vincenzo De Vivo <deltavi@users.noreply.github.com>
2020-02-09 13:41:45 +0100

    Create runkit-example.js

---
+++ runkit-example.js
@@ -0,0 +1,10 @@
+const _s = require("easy-object-selector");
+const obj = {
+     a: {
+         b: {
+             c: "val1"
+         }
+     }
+};
+const value = _s.select(obj, "a.b.c"); // => "val1"
+console.log(value);
